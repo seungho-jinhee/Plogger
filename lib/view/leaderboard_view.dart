9750928f41c68f69cc3214ef0ed9c820a6083b5e
장승호 <hoosong0235@kaist.ac.kr>
2023-06-19 02:34:24 +0900

    feat: leaderboard UI

--- lib/view/leaderboard_view.dart
+++ lib/view/leaderboard_view.dart
@@ -5,20 +5,103 @@ class LeaderboardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ColorScheme cs = Theme.of(context).colorScheme;
+    TextTheme tt = Theme.of(context).textTheme;
+
+    Widget buildCard() {
+      return Expanded(
+        child: Card(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: cs.outline),
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                const CircleAvatar(),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Seungho Jang', style: tt.titleMedium),
+                    const SizedBox(height: 4),
+                    Text('235 Picked Up', style: tt.bodyMedium),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    List<Widget> getGlobalLeaderboard() {
+      return List.generate(
+        20,
+        (index) => Column(
+          children: [
+            SizedBox(height: index == 0 ? 0 : 8),
+            Row(
+              children: [
+                Text(
+                  ((index + 1) > 9 ? '' : '0') + (index + 1).toString(),
+                  style: tt.headlineLarge,
+                ),
+                const SizedBox(width: 32),
+                buildCard(),
+              ],
+            ),
+            SizedBox(height: index == 20 - 1 ? 0 : 8),
+          ],
+        ),
+      );
+    }
+
+    Widget buildLeaderboard() {
+      return Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'My Leaderboard',
+                  style: tt.bodyLarge,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Text(
+                      '35',
+                      style: tt.headlineLarge,
+                    ),
+                    const SizedBox(width: 32),
+                    buildCard(),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Global Leaderboard',
+                  style: tt.bodyLarge,
+                ),
+                const SizedBox(height: 16),
+                ...getGlobalLeaderboard(),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return TabBarView(
       children: <Widget>[
-        Center(
-          child: Text('Total'),
-        ),
-        Center(
-          child: Text('Plastic'),
-        ),
-        Center(
-          child: Text('Glass'),
-        ),
-        Center(
-          child: Text('Metal'),
-        ),
+        buildLeaderboard(),
+        buildLeaderboard(),
+        buildLeaderboard(),
+        buildLeaderboard(),
       ],
     );
   }
